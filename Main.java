dc9bf11ca0dddb1788961dee5717ffe5a89bc7c4
Andrew DeMaria <ademaria@mines.edu>
2013-03-02 17:38:51 -0700

    Downloaded zip file

---
+++ Main.java
@@ -0,0 +1,31 @@
+import java.sql.*;
+import javax.sql.*;
+
+public class Main {
+
+    public static void main(String args[]) {
+        Main app = new Main();
+        app.run();
+    }
+
+    private void run() {
+        System.out.println("Rolling Stone's Supposed Top 100 Albums of All Time");
+        try {
+            Class.forName("org.sqlite.JDBC");
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:albums.sqlite3.db");
+            Statement stmt = conn.createStatement();
+            ResultSet results = stmt.executeQuery("SELECT * FROM albums;");
+            while (results.next()) {
+                System.out.println( results.getInt("rank") + " " + results.getString("title") + " (" + results.getInt("year") + ")");
+            }
+            conn.close();
+        } catch (ClassNotFoundException e) {
+            System.out.println("Could not load SQLite JDBC Driver.");
+            System.out.println(e.getMessage());
+        } catch (SQLException e) {
+            System.out.println("SQL Error.");
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
