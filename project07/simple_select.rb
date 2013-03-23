250272b017ec192e8f062d62a43f9ce022b75085
Andrew DeMaria <ademaria@mines.edu>
2013-03-23 12:18:40 -0600

    Simple select name query added

--- project07/simple_select.rb
+++ project07/simple_select.rb
@@ -12,6 +12,11 @@ begin
     'snacks',
     3306)
 
+  results = db.query "SELECT name FROM project_test"
+  results.each { |row|
+    puts row
+  }
+  results.free
 rescue Mysql::Error
   puts "Mysql threw an error"
 ensure
