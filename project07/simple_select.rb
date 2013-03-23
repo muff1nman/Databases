a568e2faac1456cba68a46b4a35e333045e926e9
Andrew DeMaria <ademaria@mines.edu>
2013-03-23 12:05:01 -0600

    Added error checking

--- project07/simple_select.rb
+++ project07/simple_select.rb
@@ -4,10 +4,19 @@ require 'mysql'
 
 password = 'birdsdontfly'
 
-db = Mysql::new(
-  'csci403.c99q7trvwetr.us-west-2.rds.amazonaws.com',
-  'ademaria',
-  password,
-  'snacks',
-  3306)
+begin
+  db = Mysql::new(
+    'csci403.c99q7trvwetr.us-west-2.rds.amazonaws.com',
+    'ademaria',
+    password,
+    'snacks',
+    3306)
+
+rescue Mysql::Error
+  puts "Mysql threw an error"
+ensure
+  db.close
+end
+
+
 
