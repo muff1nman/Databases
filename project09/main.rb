5e93fcd7a33f4dae4ae48c67545ee423561e6830
Andrew DeMaria <ademaria@mines.edu>
2013-04-14 10:50:16 -0600

    redis connection works just fine

--- project09/main.rb
+++ project09/main.rb
@@ -1,3 +1,11 @@
 #!/usr/bin/env ruby
 
 require 'redis'
+
+redis = Redis.new(
+  host: "pub-redis-13059.us-west-1.1.azure.garantiadata.com",
+  port: 13059,
+  password: "wuwnosql403"
+)
+
+puts redis.info
