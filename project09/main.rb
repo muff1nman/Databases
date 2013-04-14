a34327c7ece6ce97e2bc47e233acce55745a3826
Andrew DeMaria <ademaria@mines.edu>
2013-04-14 10:59:32 -0600

    Tested simple get and put

--- project09/main.rb
+++ project09/main.rb
@@ -1,6 +1,20 @@
 #!/usr/bin/env ruby
 
 require 'redis'
+require 'json'
+
+def display_divider
+  20.times { print "=" }
+  puts ""
+end
+
+def display_section( string )
+  puts string
+  display_divider
+  yield
+  display_divider
+end
+
 
 redis = Redis.new(
   host: "pub-redis-13059.us-west-1.1.azure.garantiadata.com",
@@ -8,4 +22,10 @@ redis = Redis.new(
   password: "wuwnosql403"
 )
 
-puts redis.info
+display_section "Testing a simple storage of a value" do
+  puts redis.set "somevalue", 23
+
+  puts redis.get "somevalue"
+
+end
+
