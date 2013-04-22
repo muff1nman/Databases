4d1505c8f42c89faa68b316a209962be985e58ce
Andrew DeMaria <ademaria@mines.edu>
2013-04-21 19:03:32 -0600

    Lots of printing added

--- project10/zoolicious.rb
+++ project10/zoolicious.rb
@@ -7,8 +7,56 @@
 require 'mongo'
 include Mongo
 
+def display_divider
+  divider = ""
+  20.times { divider << "=" }
+  divider
+end
+
+def display_section( string )
+  puts string
+  puts display_divider
+  yield
+  puts display_divider
+  puts "\n\n"
+end
+
 db = MongoClient.new("staff.mongohq.com", 10033).db("zoolicious")
 
 db.authenticate( ENV['mongo_user'], ENV['mongo_pass'] )
 
-puts db.collection_names
+# Your program should list the zoo names, habitat names and descriptions, and
+# the animal names, descriptions and cuteness values.
+display_section "Printing all the zoo names" do
+  db["zoos"].find.each do |zoo|
+     puts zoo["name"]
+  end
+end
+
+display_section "Printing the habitat names from the habitats collection" do
+  db["habitats"].find.each do |habitat|
+    puts "#{habitat["name"]} : #{habitat["description"]}"
+  end
+end
+
+display_section "Printing the animal names from the animals collection" do
+  db["animals"].find.each do |animal|
+    habitat = db["habitats"].find_one " _id" => animal["habitat_id"] 
+    puts "#{animal["name"]}"
+    20.times { print "-" }
+    puts ""
+    puts " Cuteness: #{animal["cuteness"]}" if animal["cuteness"]
+    puts " Description: #{animal["description"]}" if animal["description"]
+    puts " Habitat: #{habitat["name"]}" if habitat
+    puts ""
+  end
+end
+
+
+# In addition, you will ﬁnd that some animals are associated with particular
+# habitats. Your list of animals should also display the habitats associated
+# with each animal, if such a relationship exists. (How might you use the
+# “object id’s” to display the associated habitats?) 
+
+# Lastly, your program should prompt the user for an animal name, description,
+# and cuteness value (an integer), and store a new animal in the database.
